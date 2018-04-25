7926a96fcb9d202dc70d22aeb317e72abe150f6f
Peter Moorthamer <peter@moorthamer.be>
2018-04-25 14:22:22 +0200

    Change packaging to war

--- src/main/java/com/jnj/honeur/catalogue/CatalogueApplication.java
+++ src/main/java/com/jnj/honeur/catalogue/CatalogueApplication.java
@@ -2,9 +2,16 @@ package com.jnj.honeur.catalogue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CatalogueApplication {
+public class CatalogueApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CatalogueApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogueApplication.class, args);
